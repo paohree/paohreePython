74cacea3f0a2245218e75526bd8af1054175dbba
paohree <zzangyw1010@icloud.com>
2023-07-31 12:31:54 +0900

    hit

---
+++ python_work/part1_chapter4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas=["potato pizza", "bacon pizza", "pineapple pizza"]
+for pizza in pizzas:
+    #print(pizza.title())
+    print(f"I like {pizza}")
+print("I love pizza!")
---
+++ python_work/part1_chapter4/square_numbers.py
@@ -0,0 +1,13 @@
+squares=[]
+for value in range(1,11):
+    #square=value ** 2
+    #squares.append(square)
+    
+    squares.append(value**2)
+    
+print(squares)
+
+digits=[1,2,3,4,5,6,7,8,9,0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
