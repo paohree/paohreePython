92d2db70cfc99ba8c70e56b0e1d97a0c4e68c0a6
paohree <zzangyw1010@icloud.com>
2023-08-03 21:24:25 +0900

    time is running out

---
+++ python_work/part1_chapter5/conditional_test.py
@@ -0,0 +1,18 @@
+sentense1="wowdoge"
+
+print(sentense1=="Wowdoge")
+print(sentense1!="Wowdoge")
+
+print(sentense1.lower()=="Wowdoge")
+print(sentense1.lower()!="Wowdoge")
+
+number=22
+
+print(number==20)
+print(number!=20)
+print(number>20 and number<30)
+print(number>=20 or number<=30)
+
+box=['bitcoin','ethereum','solana']
+print('bitcoin' in box)
+print('bitcoin' not in box)
